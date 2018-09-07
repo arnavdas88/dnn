e08e777b698c9c31be64b766b4ccc91efcab60db
volgunin <avolgunin@gmail.com>
2018-09-07 11:41:59 -0600

    int.MulDiv method

--- src/Genix.Core/System/Int32Extensions.cs
+++ src/Genix.Core/System/Int32Extensions.cs
@@ -7,12 +7,35 @@
 namespace System
 {
     using System.Runtime.CompilerServices;
+    using Genix.Win32;
 
     /// <summary>
     /// Provides extension methods for the <see cref="int"/> class.
     /// </summary>
     public static class Int32Extensions
     {
+        /// <summary>
+        /// Multiplies two 32-bit integers and then divides the 64-bit result by a third 32-bit value.
+        /// </summary>
+        /// <param name="value">The multiplicand.</param>
+        /// <param name="numerator">The multiplier.</param>
+        /// <param name="denominator">The number by which the result of the multiplication operation is to be divided.</param>
+        /// <returns>
+        /// <para>
+        /// If the function succeeds, the return value is the result of the multiplication and division, rounded to the nearest integer.
+        /// If the result is a positive half integer (ends in .5), it is rounded up.
+        /// If the result is a negative half integer, it is rounded down.
+        /// </para>
+        /// <para>
+        /// If either an overflow occurred or <c>denominator</c> was 0, the return value is -1.
+        /// </para>
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int MulDiv(this int value, int numerator, int denominator)
+        {
+            return Win32NativeMethods.MulDiv(value, numerator, denominator);
+        }
+
         /// <summary>
         /// Determines whether this instance falls within the range of specified 32-bit signed integers.
         /// </summary>
--- src/Genix.Core/Win32/Win32NativeMethods.cs
+++ src/Genix.Core/Win32/Win32NativeMethods.cs
@@ -58,5 +58,24 @@ namespace Genix.Win32
         public static extern void memset(IntPtr dst, int value, IntPtr size);
 
 #pragma warning restore SA1300 // Element should begin with upper-case letter
+
+        /// <summary>
+        /// Multiplies two 32-bit integers and then divides the 64-bit result by a third 32-bit value.
+        /// </summary>
+        /// <param name="number">The multiplicand.</param>
+        /// <param name="numerator">The multiplier.</param>
+        /// <param name="denominator">The number by which the result of the multiplication operation is to be divided.</param>
+        /// <returns>
+        /// <para>
+        /// If the function succeeds, the return value is the result of the multiplication and division, rounded to the nearest integer.
+        /// If the result is a positive half integer (ends in .5), it is rounded up.
+        /// If the result is a negative half integer, it is rounded down.
+        /// </para>
+        /// <para>
+        /// If either an overflow occurred or <c>denominator</c> was 0, the return value is -1.
+        /// </para>
+        /// </returns>
+        [DllImport(Win32NativeMethods.KERNEL32)]
+        public static extern int MulDiv(int number, int numerator, int denominator);
     }
 }
